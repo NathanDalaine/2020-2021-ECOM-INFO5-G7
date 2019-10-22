ca06df6eeca6c29ec7f4acd834f642b58f191df6
Tom SOLVERY <solverytom@hotmail.fr>
2019-10-22 15:13:44 +0200

    Planche + voiles

--- app/src/main/webapp/app/pages/materialList/materialList.module.ts
+++ app/src/main/webapp/app/pages/materialList/materialList.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { EcomgucvoileSharedModule } from 'app/shared/shared.module';
 import { MATERIAL_LIST_ROUTE } from './materialList.route';
 import { MaterialListComponent } from './materialList.component';
+import { TableModule } from 'primeng/table'
 
 @NgModule({
-  imports: [EcomgucvoileSharedModule, RouterModule.forChild([MATERIAL_LIST_ROUTE])],
+  imports: [EcomgucvoileSharedModule, RouterModule.forChild([MATERIAL_LIST_ROUTE]),TableModule],
   declarations: [MaterialListComponent]
 })
 export class EcomgucvoileMaterialListModule {}
