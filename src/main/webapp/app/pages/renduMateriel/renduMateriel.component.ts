fe46f67d502eaf93a17228d3772d4b5403d27577
nathandalaine <nathan.dalaine@orange.fr>
2019-11-26 09:36:47 +0100

    frontend rendu Materiel

--- src/main/webapp/app/pages/renduMateriel/renduMateriel.component.ts
+++ src/main/webapp/app/pages/renduMateriel/renduMateriel.component.ts
@@ -3,11 +3,11 @@ import { AccountService } from 'app/core/auth/account.service';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { filter, map } from 'rxjs/operators';
 import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
-import {IReservation, Reservation} from "app/shared/model/reservation.model";
-import {ReservationService} from "app/entities/reservation/reservation.service";
-import * as moment from "moment";
-import {Observable} from "rxjs";
-import {IUserProfile} from "app/shared/model/user-profile.model";
+import { IReservation, Reservation } from 'app/shared/model/reservation.model';
+import { ReservationService } from 'app/entities/reservation/reservation.service';
+import * as moment from 'moment';
+import { Observable } from 'rxjs';
+import { IUserProfile } from 'app/shared/model/user-profile.model';
 
 @Component({
   selector: 'jhi-rendumateriel',
@@ -15,9 +15,9 @@ import {IUserProfile} from "app/shared/model/user-profile.model";
   styleUrls: ['renduMateriel.scss']
 })
 export class RenduMaterielComponent implements OnInit, OnDestroy {
-  reservations : IReservation[];
+  reservations: IReservation[];
   reservation: IReservation;
-  reservationNonRendu : IReservation[];
+  reservationNonRendu: IReservation[];
   currentAccount: any;
   private success: boolean;
 
@@ -51,32 +51,38 @@ export class RenduMaterielComponent implements OnInit, OnDestroy {
     });
   }
 
-
   ngOnDestroy() {}
 
-  confirm(){
+  confirmAll() {
     this.reservation.dateRendu = moment();
     this.subscribeToSaveResponse(this.reservationService.update(this.reservation));
   }
 
-  getReservationNonRendu(){
-    this.reservations.forEach(res =>{
-      if(res.dateRendu == null){
+  confirm() {
+    this.reservation.dateRendu = moment();
+    this.subscribeToSaveResponse(this.reservationService.update(this.reservation));
+  }
+
+  getReservationNonRendu() {
+    this.reservations.forEach(res => {
+      if (res.dateRendu == null) {
         this.reservationNonRendu.push(res);
       }
     });
   }
 
   protected subscribeToSaveResponse(result: Observable<HttpResponse<IReservation>>) {
-    result.subscribe(() => {
-      this.success = true;
-      this.onSuccess("ecomgucvoileApp.renduMateriel.validation")
-    }, () => {
-      this.success = false;
-      this.onError("Rendu Impossible");
-    });
+    result.subscribe(
+      () => {
+        this.success = true;
+        this.onSuccess('ecomgucvoileApp.renduMateriel.validation');
+      },
+      () => {
+        this.success = false;
+        this.onError('Rendu Impossible');
+      }
+    );
   }
-
 
   protected onError(errorMessage: string) {
     this.jhiAlertService.error(errorMessage, null, null);
