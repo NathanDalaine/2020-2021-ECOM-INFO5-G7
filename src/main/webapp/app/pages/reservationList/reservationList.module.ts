b95d6d32421983b4a68198a7ceece99a58356622
Maxence BRÈS <contact@maxence-bres.fr>
2019-12-10 03:40:55 +0100

    Corrects error that were preventing the app to be deployed in prod

--- src/main/webapp/app/pages/reservationList/reservationList.module.ts
+++ src/main/webapp/app/pages/reservationList/reservationList.module.ts
@@ -4,12 +4,13 @@ import { RouterModule } from '@angular/router';
 import { EcomgucvoileSharedModule } from 'app/shared/shared.module';
 import { RESERVATIONLIST_ROUTE } from './reservationList.route';
 import { ReservationListComponent } from './reservationList.component';
+import { ValuesPipe } from 'app/pages/reservationList/value.pipe';
 import { ValuesFullPipe } from 'app/pages/reservationList/valuefull.pipe';
-import {ValuesHistoryPipe} from "app/pages/reservationList/valuehistory.pipe";
-import {MatTabsModule} from "@angular/material/tabs";
+import { ValuesHistoryPipe } from 'app/pages/reservationList/valuehistory.pipe';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   imports: [EcomgucvoileSharedModule, RouterModule.forChild([RESERVATIONLIST_ROUTE]), MatTabsModule],
-  declarations: [ReservationListComponent, ValuesFullPipe,ValuesHistoryPipe]
+  declarations: [ReservationListComponent, ValuesFullPipe, ValuesHistoryPipe, ValuesPipe]
 })
 export class EcomgucvoileReservationListModule {}
