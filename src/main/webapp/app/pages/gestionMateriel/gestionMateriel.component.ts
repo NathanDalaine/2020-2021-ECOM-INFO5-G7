a8ee36124ed960dbcf222333b38614c3c72c07be
Jérémy <jeremy.masson1@etu.univ-grenoble-alpes.fr>
2019-12-11 18:40:59 +0100

    Starting the management of damaged equipment

--- src/main/webapp/app/pages/gestionMateriel/gestionMateriel.component.ts
+++ src/main/webapp/app/pages/gestionMateriel/gestionMateriel.component.ts
@@ -1,25 +1,74 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AccountService } from 'app/core/auth/account.service';
-import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import { filter, map } from 'rxjs/operators';
-import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
-import { IReservation, Reservation } from 'app/shared/model/reservation.model';
-import { ReservationService } from 'app/entities/reservation/reservation.service';
-import * as moment from 'moment';
-import { Observable } from 'rxjs';
-import { IUserProfile } from 'app/shared/model/user-profile.model';
-import { IReservationFull } from 'app/shared/model/reservationFull.model';
-import { ActivatedRoute } from '@angular/router';
-import { UserProfileService } from 'app/entities/user-profile/user-profile.service';
-import { TypeAbonnement } from 'app/shared/model/enumerations/type-abonnement.model';
+import { Component, OnInit} from '@angular/core';
+import {IPlanche} from "app/shared/model/planche.model";
+import {IVoile} from "app/shared/model/voile.model";
+import {VoileService} from "app/entities/voile/voile.service";
+import {filter, map} from "rxjs/operators";
+import {HttpErrorResponse, HttpResponse} from "@angular/common/http";
+import {JhiAlertService} from "ng-jhipster";
 
 @Component({
-  selector: 'jhi-gestionMateriel',
+  selector: 'jhi-gestionmateriel',
   templateUrl: './gestionMateriel.component.html',
   styleUrls: ['gestionMateriel.scss']
 })
 export class GestionMaterielComponent implements OnInit {
-  constructor() {}
+  allPlanches: IPlanche[];
+  allVoiles: IPlanche[];
+  voiles: IVoile[];
+  planches: IPlanche[];
+
+
+  constructor(protected voileService : VoileService, protected jhiAlertService: JhiAlertService) {}
+
+  loadAll() {
+    this.voileService
+      .queryDamaged()
+      .pipe(
+        filter((res: HttpResponse<IVoile[]>) => res.ok),
+        map((res: HttpResponse<IVoile[]>) => res.body)
+      )
+      .subscribe(
+        (res: IVoile[]) => {
+          this.allVoiles = res;
+          this.voiles = res;
+        },
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+  }
 
   ngOnInit() {}
+
+  searchPlanche(text: string) {
+    if (text !== '') {
+      const regex = RegExp(text.toLowerCase());
+      this.planches = this.allPlanches.filter(res => {
+        return regex.exec(res.libelle.toLocaleLowerCase());
+      });
+    } else {
+      this.planches = this.allPlanches;
+    }
+  }
+
+  searchVoile(text: string) {
+    if (text !== '') {
+      const regex = RegExp(text.toLowerCase());
+      this.voiles = this.allVoiles.filter(res => {
+        return regex.exec(res.libelle.toLocaleLowerCase());
+      });
+    } else {
+      this.voiles = this.allVoiles;
+    }
+  }
+
+  trackIdVoile(index: number, item: IVoile) {
+    return item.id;
+  }
+
+  trackIdPlanche(index: number, item: IPlanche) {
+    return item.id;
+  }
+
+  protected onError(errorMessage: string) {
+    this.jhiAlertService.error(errorMessage, null, null);
+  }
 }
--- src/main/webapp/app/pages/gestionMateriel/gestionMateriel.route.ts
+++ src/main/webapp/app/pages/gestionMateriel/gestionMateriel.route.ts
@@ -1,17 +1,13 @@
-import { ActivatedRouteSnapshot, Resolve, Route, RouterStateSnapshot } from '@angular/router';
+import {Route } from '@angular/router';
 
 import { GestionMaterielComponent } from './gestionMateriel.component';
 import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-import { HttpResponse } from '@angular/common/http';
 
 export const GESTION_MATERIEL_ROUTE: Route = {
-  path: 'reinscription',
+  path: 'gestionMateriel',
   component: GestionMaterielComponent,
   data: {
-    authorities: ['ROLE_USER', 'ROLE_ADMIN', 'ROLE_GESTIONNAIRE'],
+    authorities: ['ROLE_ADMIN', 'ROLE_GESTIONNAIRE'],
     pageTitle: 'ecomgucvoileApp.gestionMateriel.home.title'
   },
   canActivate: [UserRouteAccessService]
